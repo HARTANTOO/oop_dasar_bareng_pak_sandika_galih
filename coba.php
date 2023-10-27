d3a27c5dee8b695cc2881e9ef67824e23fbd9246
HARTANTOO <86630615+HARTANTOO@users.noreply.github.com>
2023-10-27 22:29:23 +0700

    memulai belajar seri baru oop php

---
+++ coba.php
@@ -0,0 +1,33 @@
+<?php
+
+//coba oop
+class Coba
+{
+}
+
+$a = new coba();
+$b = new coba();
+$c = new coba();
+
+var_dump($c);
+
+//class toko
+class Toko
+{
+}
+
+$toko1 = new toko();
+$toko2 = new toko();
+$toko3 = new toko();
+
+var_dump($toko3);
+
+//class pesawat
+class Pesawat
+{
+}
+
+$boeng737 = new Pesawat();
+$jet55 = new Pesawat();
+
+var_dump($jet55);
---
+++ produk.php
@@ -0,0 +1,12 @@
+<?php
+
+//membuat produk
+class Produk
+{
+}
+
+$produk1 = new Produk();
+$produk2 = new Produk();
+$produk3 = new Produk();
+
+var_dump($produk3);
